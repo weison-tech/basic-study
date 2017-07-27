6e8040ca1b05aa78396339ac4e90ff1dcd920e1d
xiaomalover <xiaomalover@gmail.com>
2017-07-27 22:52:12 +0800

    update

--- protected/core/config/asset.php
+++ protected/core/config/asset.php
@@ -4,20 +4,18 @@
  */
 
 // In the console environment, some path aliases may not exist. Please define these:
-Yii::setAlias('@webroot', __DIR__ . '/../web');
+Yii::setAlias('@webroot', realpath(__DIR__ . '/../../../'));
 Yii::setAlias('@web', '/');
 
 return [
-    'jsCompressor' => 'gulp compress-js --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
-    'cssCompressor' => 'gulp compress-css --gulpfile tools/gulp/gulpfile.js --src {from} --dist {to}',
+    'jsCompressor' => 'gulp compress-js --gulpfile ./../tools/gulp/gulpfile.js --src {from} --dist {to}',
+    'cssCompressor' => 'gulp compress-css --gulpfile ./../tools/gulp/gulpfile.js --src {from} --dist {to}',
     // Whether to delete asset source after compression:
     'deleteSource' => false,
     // The list of asset bundles to compress:
     // Asset bundle for compression output:
     'bundles' => [
-        'app\assets\PageOneAsset',
-        'app\assets\PageTwoAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'core\modules\admin\assets\AdminLteAsset',
     ],
     'targets' => [
         'allPage' => [
@@ -26,18 +24,13 @@ return [
             'baseUrl' => '@web/assets',
             'js' => 'all-page-{hash}.js',
             'css' => 'all-page-{hash}.css',
-            'depends' => [
-                'app\assets\PageOneAsset',
-                'app\assets\PageTwoAsset',
-                'yii\bootstrap\BootstrapAsset',
-            ],
         ],
     ],
 
     // Asset manager configuration:
     'assetManager' => [
-        'basePath' => '@webroot',
-        'baseUrl' => '@web',
+        'basePath' => '@webroot/assets',
+        'baseUrl' => '@web/assets',
     ],
 
 ];
